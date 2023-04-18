9814c3c383b1dbbee2a4729d98c9b4e22ba3eca2
teepheh-git <teepheh@yahoo.com>
2023-04-18 13:09:55 +0100

    bottom tab done

--- src/navigation/BottomTabs.js
+++ src/navigation/BottomTabs.js
@@ -5,6 +5,7 @@ import Home from "../screens/Home";
 import Favorites from "../screens/Favorites";
 import Chats from "../screens/Chats";
 import Profile from "../screens/Profile";
+import { Feather, AntDesign, Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,12 +14,122 @@ const BottomTabs = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 100,
+          paddingHorizontal: 20,
+          //   https://ethercreative.github.io/react-native-shadow-generator/
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.23,
+          shadowRadius: 2.62,
+          elevation: 4,
+        },
       }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Favorites" component={Favorites} />
-      <Tab.Screen name="Chats" component={Chats} />
-      <Tab.Screen name="Settings" component={Profile} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return focused ? (
+              <View
+                style={{
+                  backgroundColor: "#9D69FC",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingHorizontal: 15,
+                  paddingVertical: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="home" size={24} color="white" />
+                <Text style={{ color: "white", marginLeft: 5 }}>Home</Text>
+              </View>
+            ) : (
+              <Feather name="home" size={24} color="black" />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return focused ? (
+              <View
+                style={{
+                  backgroundColor: "#9D69FC",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingHorizontal: 15,
+                  paddingVertical: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <AntDesign name="hearto" size={24} color="white" />
+                <Text style={{ color: "white", marginLeft: 5 }}>Fav</Text>
+              </View>
+            ) : (
+              <AntDesign name="hearto" size={24} color="black" />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={Chats}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return focused ? (
+              <View
+                style={{
+                  backgroundColor: "#9D69FC",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingHorizontal: 15,
+                  paddingVertical: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Ionicons name="chatbubble-outline" size={24} color="white" />
+                <Text style={{ color: "white", marginLeft: 5 }}>Chat</Text>
+              </View>
+            ) : (
+              <Ionicons name="chatbubble-outline" size={24} color="black" />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return focused ? (
+              <View
+                style={{
+                  backgroundColor: "#9D69FC",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingHorizontal: 15,
+                  paddingVertical: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="user" size={24} color="white" />
+                <Text style={{ color: "white", marginLeft: 5 }}>Profile</Text>
+              </View>
+            ) : (
+              <Feather name="user" size={24} color="black" />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
